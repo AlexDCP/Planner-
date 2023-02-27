0d8592adb8ac8482f0d886efdae636d0bdc05e77
AlexDCP <alexdcmarriot@gmail.com>
2023-02-26 17:02:04 -0800

    Got the date working

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,4 @@
-var dateDisplay = $('#currentDay');
-
-function displayTime() {
-  var currentTime = dayjs().format('MMMM DDDD, D');
-  dateDisplay.text(currentTime);
-}
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -28,4 +16,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+
+var dateDisplay = $('#currentDay');
+
+// The document.ready function tells the webpage to start the jQuery after the HTML has been rendered.
+$(document).ready(function () {
+
+  var currentTime = dayjs().format('dddd, MMMM D');
+  dateDisplay.text(currentTime);
+
+  
+  });
